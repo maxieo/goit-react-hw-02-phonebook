fcc84e48780245760a54444b7bf14d47f4ed095f
maxieo <97198812+maxieo@users.noreply.github.com>
2022-10-20 16:33:02 +0300

    final

---
+++ src/components/ContactItem/ContactItem.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+
+const ContactItem = ({ id, name, number, onDelete }) => {
+  return (
+    <ul>
+      <p>
+        {name}: {number}
+      </p>
+      <button type="button" onClick={() => onDelete(id)}>
+        Delete Contact
+      </button>
+    </ul>
+  )
+}
+
+ContactItem.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.exact ({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+      onDelete: PropTypes.func.isRequired
+    })
+  )
+}
+
+export default ContactItem
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from 'prop-types'
+import ContactItem from "components/ContactItem/ContactItem";
+
+const ContactList = ({ contacts, onDelete }) => { 
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) =>
+        <ContactItem
+          key={id}
+          id={id}
+          name={name}
+          number={number}
+          onDelete={onDelete}
+        />
+      )}
+    </ul>
+  )
+}
+
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.exact({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+      }
+    )
+  )
+}
+
+export default ContactList
